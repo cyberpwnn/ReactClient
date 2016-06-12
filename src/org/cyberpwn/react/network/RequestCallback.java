aa1c3da55c3451fd2d2bab590cce9ebd3c5c91b8
Daniel Mills <danielmillst@gmail.com>
2016-06-12 18:33:10 -0400

    Request thread callback

--- src/org/cyberpwn/react/network/RequestCallback.java
+++ src/org/cyberpwn/react/network/RequestCallback.java
@@ -1,10 +1,31 @@
 package org.cyberpwn.react.network;
 
+import org.cyberpwn.react.util.GMap;
+
 public class RequestCallback implements Runnable
 {
+	private GMap<String, Double> data;
+	private boolean ok;
+	
+	public void run(GMap<String, Double> data, boolean ok)
+	{
+		this.data = data;
+		this.ok = ok;
+	}
+	
 	@Override
 	public void run()
 	{
 		
+	}
+
+	public GMap<String, Double> getData()
+	{
+		return data;
+	}
+
+	public boolean isOk()
+	{
+		return ok;
 	}
 }
