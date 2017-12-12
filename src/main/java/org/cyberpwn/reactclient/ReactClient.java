f030308659801a3f4b5ff54ae51cab038fa0fe85
Dan <cyberpwnn@users.noreply.github.com>
2017-12-12 12:09:47 -0500

    rc

--- src/main/java/org/cyberpwn/reactclient/ReactClient.java
+++ src/main/java/org/cyberpwn/reactclient/ReactClient.java
@@ -1,9 +1,22 @@
 package org.cyberpwn.reactclient;
 
+import org.cyberpwn.reactlink.RemoteClient;
+
 public class ReactClient
 {
 	public static void main(String[] a)
 	{
+		startClient();
+	}
 
+	public static void startClient()
+	{
+		RemoteClient client = new RemoteClient("localhost", 8192);
+
+	}
+
+	public static void l(String log)
+	{
+		System.out.println(log);
 	}
 }
